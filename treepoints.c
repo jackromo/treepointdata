4cd09e6cc290a90a05f41ad5e41081dbd2b2fd1b
Jack Romo <sharrackor@gmail.com>
2018-05-03 12:35:40 +0100

    Add tree_pointdata_init initial definition

--- treepoints.c
+++ treepoints.c
@@ -1,5 +1,35 @@
 #include "treepoints.h"
 #include <stdio.h>
+#include <errno.h>
+
+
+ssize_t _readline (char *out, size_t *len, size_t maxlen, FILE *fp)
+{
+  char linebuf[maxlen];
+  int i;
+  char c = fgetc (fp);
+  linebuf[0] = c;  /* If line is 1 character long at end of file */
+
+  for (; (!feof (fp)) && (c != '\n'); c = fgetc (fp))
+  {
+    /*
+     * We choose to fail if line exceeds a given length,
+     * since we can reasonably guess line length in this case.
+     */
+    if (i >= maxlen)
+      return -1;
+    linebuf[i] = c;
+    i++;
+  }
+
+  if ((out = malloc (sizeof(char)*(i+1))) == NULL)
+    return -1;
+
+  for (int j = 0; j < i; j++)
+    out[j] = linebuf[j];
+
+  return 0;
+}
 
 
 /*
@@ -11,7 +41,107 @@
 tree_pointdata_t *
 tree_pointdata_init (const char *path)
 {
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  tree_pointdata_t *data;
+  int coordlen;
+  int curr_coordlen = COORD_LIST_SIZE;
+  int max_z, min_z;
 
+  /* Allocate data memory. */
+
+  _safe_malloc (data, sizeof(tree_pointdata_t))
+  _safe_malloc (data->xs, sizeof(tree_pointdata_t)*curr_coordlen)
+  _safe_malloc (data->ys, sizeof(tree_pointdata_t)*curr_coordlen)
+  _safe_malloc (data->zs, sizeof(tree_pointdata_t)*curr_coordlen)
+
+  FILE *inp_file = fopen (path, "r");
+  if (inp_file == NULL)
+    _EXIT_FAIL ("Error in tree_pointdata_init on reading file")
+
+  /* Read file line-by-line for coordinates. */
+  while ((read = _readline (&line, &len, 255, inp_file)) != -1)
+  {
+    int x, y, z;
+    sscanf (line, "%d, %d, %d", &x, &y, &z);
+
+    if (coordlen >= curr_coordlen)
+    {
+      /* Reallocate all coord lists to double length. */
+      curr_coordlen << 1;
+
+      _safe_realloc (data->xs, sizeof(tree_pointdata_t)*curr_coordlen)
+      _safe_realloc (data->ys, sizeof(tree_pointdata_t)*curr_coordlen)
+      _safe_realloc (data->zs, sizeof(tree_pointdata_t)*curr_coordlen)
+    }
+
+    data->xs[coordlen] = x;
+    data->ys[coordlen] = y;
+    data->zs[coordlen] = z;
+
+    /* Compute max and min z values as we go. */
+    if ((coordlen == 0) || (max_z < z))
+      max_z = z;
+    if ((coordlen == 0) || (min_z > z))
+      min_z = z;
+
+    coordlen++;
+    free (line);
+  }
+
+  data->num_coords = coordlen;
+  data->max_z = max_z;
+  data->min_z = min_z;
+
+  if (line)
+    free (line);
+
+  if (fclose (inp_file) == EOF)
+    _EXIT_FAIL ("Error in tree_pointdata_init on closing file")
+
+  /* Compute buckets for x and y. */
+
+  size_t num_buckets = (size_t) ((max_z - min_z) / ZBUCKET_RANGE);
+
+  _safe_malloc (data->x_z_bucket, sizeof(double *) * num_buckets)
+  _safe_malloc (data->y_z_bucket, sizeof(double *) * num_buckets)
+  _safe_malloc (data->z_bucket_lengths, sizeof(unsigned int) * num_buckets)
+
+  unsigned int curr_bucket_lengths[num_buckets];
+
+  for (int j = 0; j < (int) ((max_z - min_z) / ZBUCKET_RANGE); j++)
+  {
+    _safe_malloc (data->x_z_bucket[j], sizeof(double) * ZBUCKET_SIZE)
+    _safe_malloc (data->y_z_bucket[j], sizeof(double) * ZBUCKET_SIZE)
+    data->z_bucket_lengths[j] = 0;
+  }
+
+  for (int j = 0; j < coordlen; j++)
+  {
+    double x = data->xs[j];
+    double y = data->ys[j];
+    double z = data->zs[j];
+
+    unsigned int bucket = (int) (z / ZBUCKET_RANGE);
+    unsigned int in_bucket_pos = data->z_bucket_lengths[bucket];
+
+    if (in_bucket_pos > curr_bucket_lengths[bucket])
+    {
+      curr_bucket_lengths[bucket] << 1;
+      _safe_realloc (data->x_z_bucket[bucket],
+          sizeof(double) * curr_bucket_lengths[bucket])
+      _safe_realloc (data->y_z_bucket[bucket],
+          sizeof(double) * curr_bucket_lengths[bucket])
+    }
+
+    data->x_z_bucket[bucket][in_bucket_pos] = x;
+    data->y_z_bucket[bucket][in_bucket_pos] = y;
+
+    data->z_bucket_lengths[bucket]++;
+  }
+
+  return data;
 }
 
 double
--- treepoints.h
+++ treepoints.h
@@ -1,6 +1,25 @@
 #ifndef TREEPOINT_DATA_H
 #define TREEPOINT_DATA_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Define this as a standard way to fail. */
+#define _EXIT_FAIL(msg_prefix) { \
+    perror (msg_prefix); \
+    exit(EXIT_FAILURE); \
+  }
+
+/* Helper macros to safely allocate/reallocate memory. */
+#define _safe_malloc(ptr, sz, errmsg_prefix) { \
+  if ((ptr = malloc (sz)) == NULL) \
+    _EXIT_FAIL ("Error in _safe_malloc on allocating memory") \
+  }
+
+#define _safe_realloc(ptr, sz, errmsg_prefix) { \
+  if ((ptr = realloc (sz)) == NULL) \
+    _EXIT_FAIL ("Error in _safe_realloc on reallocating memory") \
+  }
 
 /*
  * tree_pointdata_t: Container datatype for all
@@ -8,25 +27,36 @@
  */
 
 typedef struct tree_pointdata {
-  // Dynamic arrays of x-coordinates, y-coordinates and
-  // z-coordinates respectively.
+  unsigned int num_coords;
+  /*
+   * Dynamic arrays of x-coordinates, y-coordinates and
+   * z-coordinates respectively.
+   */
   double *xs;
   double *ys;
   double *zs;
-  // Cheaper to have these from the start, as they are
-  // used repeatedly.
+  /* Initial size for coordinate list. */
+#define COORD_LIST_SIZE 1000
+  /*
+   * Cheaper to have these from the start, as they are
+   * used repeatedly.
+   */
   double max_z;
   double min_z;
-  // Buckets for X-coordinates and Y-coordinates of all
-  // points in certain ranges of Z-values, respectively.
-  double *x_z_bucket;
-  double *y_z_bucket;
-  // Range of Z-values per bucket.
-#define ZBUCKET_SIZE 5;
+  /*
+   * Buckets for X-coordinates and Y-coordinates of all
+   * points in certain ranges of Z-values, respectively.
+   */
+  double **x_z_bucket;
+  double **y_z_bucket;
+  unsigned int *z_bucket_lengths;
+  /* Range of Z-values per bucket. */
+#define ZBUCKET_RANGE 0.1;
+#define ZBUCKET_SIZE 200;
 } tree_pointdata_t;
 
-
 tree_pointdata_t *tree_pointdata_init (char *);
+
 
 double tree_pointdata_get_trunkdiam (tree_pointdata_t *);
 double tree_pointdata_get_height (tree_pointdata_t *);
